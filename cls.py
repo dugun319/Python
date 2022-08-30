b62952fd59ccc2f811b8a05ae26477e0f5bccda9
HYUK <dugun319@gmail.com>
2022-08-30 14:15:07 +0900

    "v1.08301415"

---
+++ cls.py
@@ -0,0 +1,10 @@
+import os
+
+def cls():
+    os.system('cls')
+
+def cl():
+    os.system('cls')
+
+def getcwd():
+    os.getcwd()
--- hello.py
+++ hello.py
@@ -7,10 +7,7 @@ b = 3
 print(a+b)
 
 a = [1, 2, 3, 4, 5]
-
 print(a)
 
-
 a.pop()
-
 print(a)
