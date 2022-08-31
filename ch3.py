7b9afdc36dec53f42a6efb7408ace09f782dfd4f
HYUK <dugun319@gmail.com>
2022-08-31 11:01:42 +0900

    "v1.08311101"

---
+++ ch3.py
@@ -0,0 +1,12 @@
+
+def gugudan(num):
+    for i in range(2, 10):
+        print(i, "Dan is printed")
+        for j in range(1, 10):
+            print(i, "X", j, "=", i*j)
+        print()
+
+
+if __name__ :  "__main__"
+gugudan(1)
+    
