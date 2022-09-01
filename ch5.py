815db13ffe286e363c1a709bdc135905a21f598e
HYUK <dugun319@gmail.com>
2022-09-01 10:46:58 +0900

    v1.09011046

---
+++ ch5.py
@@ -0,0 +1,26 @@
+
+class Calculator:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result += num
+        return self.result
+
+    def minus(self, num):
+        self.result -= num
+        return self.result
+
+cal1 = Calculator()
+cal2 = Calculator()
+
+print(cal1.add(3))
+print(cal1.add(4))
+print(cal1.minus(3))
+print(cal1.minus(4))
+
+print(cal2.add(3))
+print(cal2.add(7))
+print(cal2.minus(3))
+print(cal2.minus(4))
+
