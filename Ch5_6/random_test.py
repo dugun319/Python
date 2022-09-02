3323196e020bbcf827f3ba52760113d2dcfedcb0
HYUK <dugun319@gmail.com>
2022-09-02 16:02:37 +0900

    v1.09021602

--- Ch5_6/random_test.py
+++ Ch5_6/random_test.py
@@ -2,27 +2,50 @@ import random
 import time
 from webbrowser import get
 
-a = random.random()
-print(a)
-print(len(str(a)))
-
-a = random.random()
-print(a)
-print(len(str(a)))
+getstr = random.getstate()
+list(getstr)
+print("\nLine_7 getstr = ", getstr[1][0])
+print()
 
 random.seed(random.random())
+getstr = random.getstate()
+list(getstr)
+print("\nLine_13 getstr = ", getstr[1][0])
+print()
+
+a = random.random()
+print(a)
+print(len(str(a)))
+
+a = random.random()
+print(a)
+print(len(str(a)))
+
+getstr = random.getstate()
+list(getstr)
+print("\nLine_26 getstr = ", getstr[1][0])
+print()
+
+
 random.seed(time.strftime("%d", time.localtime()))
 #random.seed(time.localtime())
 
 getstr = random.getstate()
 list(getstr)
-print(getstr[:8])
+print("\nLine_35 getstr = ", getstr[1][0])
+print()
 
 # print(random.setstate())
 
 # 이 메서드를 사용하여 보안 토큰을 생성해서는 안 됩니다.
 
 print(random.randbytes(3))
+
+getstr = random.getstate()
+list(getstr)
+print("\ngetstr = ", getstr[1][0])
+print()
+
 print(random.randbytes(2))
 print(random.randbytes(1))
 
@@ -40,20 +63,25 @@ random.seed()
 
 getstr = random.getstate()
 list(getstr)
-print(getstr[0])
+print("\ngetstr = ", getstr[1][0])
+print()
 
 
 print(random.randbytes(3))
 
 getstr = random.getstate()
 list(getstr)
-print(getstr[0])
+print("\ngetstr = ", getstr[1][0])
+print()
 
 
 print(random.randbytes(2))
 print(random.randbytes(1))
 
-print(random.getstate())
+getstr = random.getstate()
+list(getstr)
+print("\ngetstr = ", getstr[1][0])
+print()
 
 print(random.randrange(10))
 print(random.randrange(0, 50, 10))
@@ -76,7 +104,7 @@ print(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 print("\n\nrandom.choices\n")
 
 str = ["Life", "is", "too", "short", "You", "Need", "Python"]
-print(random.choices(str, weights=[5, 1, 1, 1, 1, 1, 1], k = 5))
+print(random.choices(str, weights=[43, 1, 1, 1, 1, 1, 1], k = 10))
 
 
 print("\n\nrandom.shuffle\n")
@@ -92,12 +120,17 @@ print(random.uniform(1, 5))
 
 
 # LOTTO / random.sample 중복 없는 반복추출
+
+print("\n\n선택은 당신의 몫입니다. 살지 말지\n")
 lottery = []
 for i in range(1, 46):
     lottery.append(i)
 print(random.sample(lottery, 6))
 
+print()
 
-
-
-
+random.seed(random.random())
+getstr = random.getstate()
+list(getstr)
+print("\ngetstr = ", getstr[1][0])
+print()
